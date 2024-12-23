68d44b68ec1d0eb58ac398f72bb593f1e9d4e2e8
s-pushkin <58189058+s-pushkin@users.noreply.github.com>
2024-12-23 04:40:25 +0300

    Update TestGetUser.java

--- src/test/java/TestGetUser.java
+++ src/test/java/TestGetUser.java
@@ -3,6 +3,7 @@ import io.restassured.response.Response;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Tag;
+import io.qameta.allure.Allure;
 
 @Tag("getUser")
 public class TestGetUser {
